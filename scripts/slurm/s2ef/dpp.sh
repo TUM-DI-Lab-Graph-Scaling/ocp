73cb0c1ac2daea643c6590ad7b9b75215d297427
d-stoll <daniel.stoll@tum.de>
2022-07-01 18:01:53 +0000

    Slurm submission scripts

---
+++ scripts/slurm/s2ef/dpp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH --mincpus=64
+#SBATCH --mem=500gb
+#SBATCH --gpus=8
+#SBATCH --time=1:00:00
+#SBATCH --nodelist=lundquist
+#SBATCH --job-name="OCP-S2EF-dpp"
+
+conda activate ocp-models
+
+export PATH=/usr/local/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-11.1/lib64:/usr/local/extras/CUPTI/lib64:/usr/local/lib:$LD_LIBRARY_PATH
+export CUDADIR=/usr/local/cuda-11.1
+export CUDA_HOME=/usr/local/cuda-11.1
+export NCCL_P2P_LEVEL=PIX
+
+srun python -u -m torch.distributed.launch --nproc_per_node=8 main.py \
+  --distributed --num-gpus 8 --mode train --config-yml configs/s2ef/all/dimenet_plus_plus/dpp.yml
